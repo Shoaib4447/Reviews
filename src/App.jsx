622044451d8251a2bfb4e4d3c055651ea87e5929
Shoaib Bhatti <125224624+Shoaib4447@users.noreply.github.com>
2025-06-22 12:28:20 +0500

    Color Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ const App = () => {
   return (
     <>
       <main className='bg-blue-300 min-h-screen flex flex-col justify-center flex-wrap items-center gap-6'>
-        <h2 className='text-6xl font-bold underline text-blue-400'>
+        <h2 className='text-5xl font-bold underline text-blue-900'>
           Our Reviews
         </h2>
         <Review />
