d678b21c996622a4443b97fa28ad251bfe0ae733
Shoaib Bhatti <125224624+Shoaib4447@users.noreply.github.com>
2025-06-22 12:26:44 +0500

    Just changed Bg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Review from "./Review";
 const App = () => {
   return (
     <>
-      <main className='bg-gray-300 min-h-screen flex flex-col justify-center flex-wrap items-center gap-6'>
+      <main className='bg-blue-300 min-h-screen flex flex-col justify-center flex-wrap items-center gap-6'>
         <h2 className='text-6xl font-bold underline text-blue-400'>
           Our Reviews
         </h2>
